2b0cde918482ca2fbd9e23c3cb75531e203b3a93
John Warwick <jwarwick@gmail.com>
2013-06-02 21:30:36 -0400

    using httpotion to get github issues

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Issues.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    []
+    [applications: [:httpotion]]
   end
 
   # Returns the list of dependencies in the format:
